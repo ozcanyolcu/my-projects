00a5a7da7e12019dcb96b662e6a6a72ac7ea415c
özcan <ozcanyolcu@gmail.com>
2022-09-28 12:51:26 +0300

    flask-01 and 02 added

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Flask!!!"
 
-@app.route('/second')
+@app.route("/second")
 def second():
-    return 'Bize Her Yer İzmit!!!!'
+    return "This is my second page!!!!"
 
-@app.route('/third/subthird')
+@app.route("/third/subthird")
 def third():
-    return 'This is the subpage of third page'
+    return "This is the subpage of third page"
 
-@app.route('/forth/<string:id>')
+@app.route("/forth/<string:id>")
 def forth(id):
-    return f'Id number of this page is {id}'
+    return f"Id number of this page is {id}"
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True) 
